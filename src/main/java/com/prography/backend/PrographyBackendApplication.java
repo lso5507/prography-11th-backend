11d2276011237bfdc1a7964bdf5019738703b204
lso5507 <leeseokwoon@naver.com>
2026-02-26 17:43:46 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/prography/backend/PrographyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.prography.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrographyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrographyBackendApplication.class, args);
+    }
+}
